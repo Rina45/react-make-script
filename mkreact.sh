3ec5c9edec842206e50101e4a28ea3f407003a7b
Rina45 <90148827+Rina45@users.noreply.github.com>
2022-03-15 15:13:18 +0200

    npm not work well

--- mkreact.sh
+++ mkreact.sh
@@ -6,7 +6,7 @@ cp -R /usr/local/react-make-script/react-template/* "$PWD/" &&
 mv babelrc .babelrc && mv gitignore .gitignore &&
 npm init -y && 
 echo "start installing..." &&
-npm install --save-dev @babel/core @babel/preset-env @babel/preset-react @testing-library/jest-dom @testing-library/react @testing-library/user-event babel-jest babel-loader html-loader html-webpack-plugin jest webpack webpack-cli webpack-dev-server &&
+# npm install --save-dev @babel/core @babel/preset-env @babel/preset-react @testing-library/jest-dom @testing-library/react @testing-library/user-event babel-jest babel-loader html-loader html-webpack-plugin jest webpack webpack-cli webpack-dev-server &&
 npm install react react-dom &&
 code $PWD &&
 echo "My react app was created to start type cd $1 npm start"
